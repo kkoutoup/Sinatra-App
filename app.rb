e60309ab9dd7774dcc13174cdaaca873e4f9b9b4
Kostas Koutoupis <kkoutoup@gmail.com>
2020-06-03 13:49:17 +0100

    Added sqlite3 dependencies and created db instance

--- app.rb
+++ app.rb
@@ -1,12 +1,30 @@
+# Dependencies
 require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require "sqlite3"
+# Better Errors
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+# db file path
+db_file_path = File.join(File.dirname(__FILE__), "data/posts_spec.rb")
+
+# create a database instance
+DB = SQLite3::Database.new(db_file_path)
+
 get '/' do
-  'Hello World'
+  erb :index # view
+end
+
+get '/about'do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
